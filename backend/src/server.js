44cf850f9dbfd9b9f8e1ce4f97b166e2855f4bd6
TinTurtle <sivagaminitish@gmail.com>
2026-02-15 15:32:17 +0530

    made changes for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,13 +11,16 @@ dotenv.config();
 
 const app = express(); // main app
 const PORT = process.env.PORT || 5001; // PORT for the server to listen on
+const __dirname = path.resolve();
 
 //middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 app.use(express.json()); // this middleware is used to get the bodies of json: req.body
 app.use(rateLimiter);
 
@@ -27,6 +31,13 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes); //telling the app use the notesRoutes to route the requests from "/api/notes"
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  //serve static files from the build folder
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 // waitiing for the db connection to be established before starting the  main app
 connectDB().then(() => {
   app.listen(PORT, () => {
